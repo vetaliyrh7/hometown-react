a15d190efd7f01ff0ba36c2162284a39a3bd9e9f
Vitaliy Kononenko <vitaliy.kononenko@freshcode.org>
2016-09-30 18:58:18 +0300

    Nav test

--- imports/ui/app.jsx
+++ imports/ui/app.jsx
@@ -1,7 +1,10 @@
 import React, { PropTypes, Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import _ from 'lodash';
+import { Link } from 'react-router';
+import Radium from 'radium';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { bubble as Menu } from 'react-burger-menu';
 injectTapEventPlugin();
 
 //SCSS files
@@ -10,19 +13,38 @@ injectTapEventPlugin();
 import PageHeader from './components/root-ui/PageHeader';
 import NavBar from './components/root-ui/nav-bar';
 
+let RadiumLink = Radium(Link);
+
 export default class App extends Component {
 
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    isMenuOpen() {
+        const {menuOpen} = this.state;
+        this.setState({
+            menuOpen: !menuOpen
+        })
     }
 
     render() {
+        const {menuOpen} = this.state;
         return (
             <div className="root">
-                <PageHeader />
-                <NavBar />
-                {this.props.children}
+                <div  id="outer-container">
+                    <Menu noOverlay isOpen={menuOpen} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+                        <NavBar />
+                    </Menu>
+                    <button className="nav-btn-open" onClick={this.isMenuOpen.bind(this)}> >> </button>
+                    <div id="page-wrap">
+                        <PageHeader />
+                        {this.props.children}
+                    </div>
+                </div>
             </div>
         );
     }
--- imports/ui/components/root-ui/nav-bar.jsx
+++ imports/ui/components/root-ui/nav-bar.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from "react";
 import { browserHistory, Link } from 'react-router';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
 import FaAngleRight from 'react-icons/lib/fa/angle-right';
+
 // Theme components
 
 export default class NavBar extends Component {
@@ -12,34 +13,23 @@ export default class NavBar extends Component {
         }
     }
 
-    toggleMenu() {
-        const { activated } = this.state;
-        this.setState({
-            activated: !activated
-        });
-    }
 
     render() {
         const { activated } = this.state;
         const showed = !!activated ? 'showed' : '';
 
         return (
-            <div className={`left-side-nav ${showed}`} onClick={this.toggleMenu.bind(this)}>
-                <ul className="nav">
-                    <li><Link to="/">Main</Link></li>
-                    <li><Link to="/education">Education</Link></li>
-                    <li><Link to="/films">Films & Theatres</Link></li>
-                    <li><Link to="/sights">Sights</Link></li>
-                    <li>
-                        <Link to="/dashboard/posts" className="settings-btn">
-                            Settings
-                        </Link>
-                    </li>
-                </ul>
-                <div className="arrows-block">
-                    {!!activated ? <FaAngleLeft /> : <FaAngleRight />}
-                </div>
-            </div>
+            <ul className="nav">
+                <li><Link to="/">Main</Link></li>
+                <li><Link to="/education">Education</Link></li>
+                <li><Link to="/films">Films & Theatres</Link></li>
+                <li><Link to="/sights">Sights</Link></li>
+                <li>
+                    <Link to="/dashboard/posts" className="settings-btn">
+                        Settings
+                    </Link>
+                </li>
+            </ul>
         );
     }
 }
