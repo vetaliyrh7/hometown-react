3eff1ab44408e5983180f1cab0c7367fb4886016
Vitaliy Kononenko <vitaliy.kononenko@freshcode.org>
2016-10-06 18:24:19 +0300

    Design update

--- imports/ui/app.jsx
+++ imports/ui/app.jsx
@@ -2,9 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import _ from 'lodash';
 import { Link } from 'react-router';
-import Radium from 'radium';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { bubble as Menu } from 'react-burger-menu';
 injectTapEventPlugin();
 
 //SCSS files
@@ -12,8 +10,7 @@ injectTapEventPlugin();
 // CUSTOM COMPONENTS
 import PageHeader from './components/root-ui/PageHeader';
 import NavBar from './components/root-ui/nav-bar';
-
-let RadiumLink = Radium(Link);
+import './components/root-ui/root.scss';
 
 export default class App extends Component {
 
@@ -24,27 +21,12 @@ export default class App extends Component {
         }
     }
 
-    isMenuOpen() {
-        const {menuOpen} = this.state;
-        this.setState({
-            menuOpen: !menuOpen
-        })
-    }
-
     render() {
-        const {menuOpen} = this.state;
         return (
             <div className="root">
-                <div  id="outer-container">
-                    <Menu noOverlay isOpen={menuOpen} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-                        <NavBar />
-                    </Menu>
-                    <button className="nav-btn-open" onClick={this.isMenuOpen.bind(this)}> >> </button>
-                    <div id="page-wrap">
-                        <PageHeader />
-                        {this.props.children}
-                    </div>
-                </div>
+                <NavBar />
+                <PageHeader />
+                {this.props.children}
             </div>
         );
     }
--- imports/ui/components/root-ui/nav-bar.jsx
+++ imports/ui/components/root-ui/nav-bar.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes, Component } from "react";
 import { browserHistory, Link } from 'react-router';
-import FaAngleLeft from 'react-icons/lib/fa/angle-left';
-import FaAngleRight from 'react-icons/lib/fa/angle-right';
+import FaBars from 'react-icons/lib/fa/bars';
 
 // Theme components
 
@@ -13,13 +12,19 @@ export default class NavBar extends Component {
         }
     }
 
+    isMenuOpen() {
+        const {activated} = this.state;
+        this.setState({
+            activated: !activated
+        })
+    }
 
     render() {
         const { activated } = this.state;
-        const showed = !!activated ? 'showed' : '';
+        const showed = activated ? 'showed' : '';
 
         return (
-            <ul className="nav">
+            <ul className={`nav ${showed}`}>
                 <li><Link to="/">Main</Link></li>
                 <li><Link to="/education">Education</Link></li>
                 <li><Link to="/films">Films & Theatres</Link></li>
@@ -29,11 +34,15 @@ export default class NavBar extends Component {
                         Settings
                     </Link>
                 </li>
+                <div className="open-menu-trigger" onClick={this.isMenuOpen.bind(this)}>
+                    <FaBars />
+                </div>
             </ul>
         );
     }
 }
 
 NavBar.propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    isShowed: PropTypes.bool
 };
